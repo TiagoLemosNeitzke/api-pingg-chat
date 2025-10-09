04dd6c340c130a87aee9cf4c77dfc93106ac4099
Rafael Lunardelli <rafael@lunardelli.me>
2025-10-09 15:40:30 -0600

    chore: config brain

---
+++ config/brain.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Root Directory
+    |--------------------------------------------------------------------------
+    |
+    | This value set's the main directory where we will create all the processes
+    | tasks, and queries.
+    |
+    */
+    'root' => app_path('Brain'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Suffix for Task, Process, and Query
+    |--------------------------------------------------------------------------
+    |
+    | When enabled (true), this setting appends a suffix to class names based
+    | on their type:
+    | - "Task" for tasks
+    | - "Process" for processes
+    | - "Query" for queries
+    |
+    */
+    'use_suffix' => env('BRAIN_USE_SUFFIX', true),
+
+    'suffixes' => [
+        'task'    => env('BRAIN_TASK_SUFFIX', 'Task'),
+        'process' => env('BRAIN_PROCESS_SUFFIX', 'Process'),
+        'query'   => env('BRAIN_QUERY_SUFFIX', 'Query'),
+    ],
+];
