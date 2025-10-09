8ea30f3064e4a3c967142aade95bd4f9dfd5cfe7
Rafael Lunardelli <rafael@lunardelli.me>
2025-10-09 16:07:34 -0600

    feat: create user process

---
+++ app/Brain/User/Processes/CreateUserProcess.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Brain\User\Processes;
+
+use App\Brain\User\Tasks\CreateUserTask;
+use App\Brain\User\Tasks\ValidateCreateUserTask;
+use Brain\Process;
+
+class CreateUserProcess extends Process
+{
+    protected array $tasks = [
+        ValidateCreateUserTask::class,
+        CreateUserTask::class,
+    ];
+}
---
+++ tests/Brain/User/Processes/CreateUserProcessTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types = 1);
+
+use App\Brain\User\Processes\CreateUserProcess;
+use App\Brain\User\Tasks\CreateUserTask;
+use App\Brain\User\Tasks\ValidateCreateUserTask;
+
+test('check list of tasks', function (): void {
+    $process = new CreateUserProcess();
+
+    expect($process->getTasks())
+        ->toBe([
+            ValidateCreateUserTask::class,
+            CreateUserTask::class,
+        ]);
+});
