3222600e5036242afe3deb5f3b778c68a1318eda
Rafael Lunardelli <rafael@lunardelli.me>
2025-10-09 16:07:13 -0600

    chore: updating project defaults

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,17 @@ declare(strict_types = 1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
+    #[\Override]
     public function register(): void
     {
         //
@@ -15,6 +22,52 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        $this->configPasswordRules();
+        $this->configUrls();
+        $this->configModels();
+        $this->configDates();
+        $this->configureCommands();
+    }
+
+    protected function configPasswordRules(): void
+    {
+        Password::defaults(function () {
+            $password = Password::min(8);
+
+            if (app()->isProduction()) {
+                $password
+                    ->mixedCase()
+                    ->letters()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised();
+            }
+
+            return $password;
+        });
+    }
+
+    protected function configUrls(): void
+    {
+        URL::forceScheme('https');
+    }
+
+    protected function configModels(): void
+    {
+        Model::unguard();
+        Model::shouldBeStrict();
+        Model::preventLazyLoading(! app()->isProduction());
+    }
+
+    protected function configDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    protected function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction(),
+        );
     }
 }
